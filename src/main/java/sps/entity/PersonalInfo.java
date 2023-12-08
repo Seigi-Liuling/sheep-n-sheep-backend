d2a9355cc3b2fcbaa630a4db04d1ecc4b57ff178
Seigi-Liuling <78367041+Seigi-Liuling@users.noreply.github.com>
2023-12-08 23:39:21 +0800

    Update PersonalInfo.java

--- src/main/java/sps/entity/PersonalInfo.java
+++ src/main/java/sps/entity/PersonalInfo.java
@@ -10,12 +10,12 @@ import lombok.Data;
 @Data
 public class PersonalInfo {
     Integer register_time = 833763600;
-    String avatar = "https://i0.hdslb.com/bfs/face/f621d4ee8b620a95fe0fe57d70be098bd2c024ee.jpg";
+    String avatar = "https://sheep.nnnk.fun/zoom_pic.jpg";
     String nick_name =  "扰动有效位能";
     Integer challenge = 444;
     Integer uid = 4444;
     Integer daily_count = 444;
-    Integer topic_count = 44444;
+    Integer topic_count = 4444;
     Integer today_state = 1;
     Integer today_ts = Math.toIntExact(System.currentTimeMillis() / 1000);
     Integer today_time = 114514;
